509ba03bcb7b08172c161d2466c23d6c5ac9acbc
Josh <91017208+J-shw@users.noreply.github.com>
2024-10-15 13:28:31 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='wdnas_client',
-    version='0.5',
+    version='0.6.0',
     packages=find_packages(),
     install_requires=[
         'requests>=2.32.3'
