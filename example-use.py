1f692505cc2def852e5a59e134dd2db16a8a43fd
Josh <91017208+J-shw@users.noreply.github.com>
2025-10-15 15:56:43 +0100

    Create example-use.py

---
+++ example-use.py
@@ -0,0 +1,22 @@
+import asyncio
+from wdnas_client import client
+
+async def main():
+    username = input("Username: ").lower()
+    password = input("Password: ")
+    host = '192.168.86.41'
+    version = 2 # Can be 2 or 5
+    
+    async with client(username, password, host, version) as wdNAS:
+        print("System Info:", await wdNAS.system_info())
+        print("Share Names:", await wdNAS.share_names())
+        print("System Status:", await wdNAS.system_status())
+        print("Network Info:", await wdNAS.network_info())
+        print("Device Info:", await wdNAS.device_info())
+        print("System Version:", await wdNAS.system_version())
+        print("Latest Version:", await wdNAS.latest_version())
+        print("Accounts:", await wdNAS.accounts())
+        print("Alerts:", await wdNAS.alerts())
+
+if __name__ == "__main__":
+    asyncio.run(main())
