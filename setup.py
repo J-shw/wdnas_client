2492fab0c011e0110e0fc6ce25d095517c1a891d
J-shw <91017208+J-shw@users.noreply.github.com>
2025-12-06 14:24:11 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='wdnas_client',
-    version='0.9.0',
+    version='0.9.3',
     packages=find_packages(),
     install_requires=[
         'aiohttp>=3.12.14'
